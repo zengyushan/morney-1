cb8bbf571dc4eaa33fbbd666c273d3706aacb9ae
zengyushan <zyszys805@139.com>
2020-10-11 11:41:19 +0800

    'svg图标封装'

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,51 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">记账</router-link>
+  <nav>
+    <router-link class="item" to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link class="item" to="/labels">
+      <Icon name="labels" />
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
-  </div>
+    <router-link class="item" to="/statistics">
+      <Icon name="statistics" />
+      统计
+    </router-link>
+  </Nav>
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/label.svg'
-console.log(x)
+
+
+
+import Icon from "@/components/Icon.vue";
 export default {
-  name: "Nav"
+  name: "Nav",
+  components: {Icon}
 }
 </script>
 
 <style scoped lang="scss">
-.nav {
+nav{
+  display: flex;
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
   border: 1px solid red;
+  flex-direction: row;
+  font-size: 12px;
+  > .item{
+    padding: 2px 0;
+    width: 33.33%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon{
+      width: 32px;
+      height: 32px;
+    }
+  }
 }
 </style>
